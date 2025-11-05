f6685c2d91c902a190d5e755d967843be8b2bc83
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-05 21:08:27 +0530

    customfakehttp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularhttp';
+  constructor(private http:HttpClient){}    //httpclient is built in service to communicate with API
+  
+  // fakeAPIURL="https://jsonplaceholder.typicode.com/todos";
+  //creating custom fake api url and set to install own server Json server.like get,patch-partial update,post,delete.
+  //using servers we can create apis
+  mylocalFakeURL= "http://localhost:3000/Employee";
+  data :any= [];
+  ngOnInit(): void {
+    // this.http.get(this.fakeAPIURL).subscribe((response)=>{
+    //   console.log(response);
+    //   this.data =response;
+    // });  //http is local variable
+    this.http.get(this.mylocalFakeURL).subscribe((response)=>{
+     console.log(response);
+    this.data =response;
+
+  });
+  }
+
 }
