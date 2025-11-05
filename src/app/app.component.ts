c65490f844bcddbfa380d2742625c0af1654b6ba
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-05 22:33:00 +0530

    fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,41 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'angularhttp';
-  constructor(private http:HttpClient){}    //httpclient is built in service to communicate with API
+fetchDataByID() {
+throw new Error('Method not implemented.');
+}
   
+  title = 'angularhttp';
+  mylocalFakeURL = "http://localhost:3000/Employee";
+  data: any= [];
+  constructor(private http: HttpClient) { }    //httpclient is built in service to communicate with API
+
   // fakeAPIURL="https://jsonplaceholder.typicode.com/todos";
   //creating custom fake api url and set to install own server Json server.like get,patch-partial update,post,delete.
   //using servers we can create apis
-  mylocalFakeURL= "http://localhost:3000/Employee";
-  data :any= [];
   ngOnInit(): void {
     // this.http.get(this.fakeAPIURL).subscribe((response)=>{
     //   console.log(response);
     //   this.data =response;
     // });  //http is local variable
-    this.http.get(this.mylocalFakeURL).subscribe((response)=>{
-     console.log(response);
-    this.data =response;
+    this.fetchData();
+  }
+  EmpData=false;
+  fetchData(){
+      this.http.get(this.mylocalFakeURL).subscribe((response) => {
+        // console.log(response);
+        this.data = response;
+        this.EmpData=true;
+    
 
-  });
+
+      });
+    }
+    fetchDataByID(element: HTMLInputElement){
+      let value=+element.value;
+      console.log(value);
+
+    }
+  
   }
 
-}
