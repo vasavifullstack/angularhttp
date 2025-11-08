2259b2f4311cafab500486ec7f3acbb44fe4e2ea
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-08 21:30:24 +0530

    https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-fetchDataByID() {
-throw new Error('Method not implemented.');
-}
   
   title = 'angularhttp';
   mylocalFakeURL = "http://localhost:3000/Employee";
   data: any= [];
+  GetID="http://localhost:3000/Employee/1"
   constructor(private http: HttpClient) { }    //httpclient is built in service to communicate with API
 
   // fakeAPIURL="https://jsonplaceholder.typicode.com/todos";
@@ -27,6 +24,7 @@ throw new Error('Method not implemented.');
     this.fetchData();
   }
   EmpData=false;
+  //all details data
   fetchData(){
       this.http.get(this.mylocalFakeURL).subscribe((response) => {
         // console.log(response);
@@ -37,11 +35,43 @@ throw new Error('Method not implemented.');
 
       });
     }
+    //getting details of specific id details
     fetchDataByID(element: HTMLInputElement){
-      let value=+element.value;
-      console.log(value);
+      let id=+element.value;
+     let  GetID=`http://localhost:3000/Employee/${id}`;
+      this.http.get(GetID).subscribe((response)=>{
+
+        let Employee=[];
+        Employee.push(response);
+        this.data=Employee;  //data display in object format but the data available loop format
+        this.EmpData=true;
+
+      })
+      //console.log(value);
 
     }
-  
+  inserestEmp(){
+    let newEmp={
+    id:7,
+    name:"Manvitha Reddy",
+    salary:1000000
+    }
+    let addURL=`http://localhost:3000/Employee`;
+    this.http.post(addURL,newEmp).subscribe((response)=>{
+      console.log(response);
+      alert("add successfully");
+      this.fetchData();
+    })
+    
   }
-
+deleteEmp(inputelement:HTMLInputElement){
+  let ID=+inputelement.value;
+  this.http
+  .delete(`http://localhost:3000/Employee/${ID}`)
+  .subscribe((response)=>{
+    console.log(response);
+    alert("delete successfully");
+    this.fetchData();
+  })
+}
+}
